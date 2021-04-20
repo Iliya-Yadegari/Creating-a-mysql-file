a6cc69248723bb1dc533e23e43468ac4288fe2d6
Iliya-Yadegari <naruto3@email.com>
2021-04-20 20:34:28 +0100

    Update Creating sql files.py

--- Creating sql files.py
+++ Creating sql files.py
@@ -29,6 +29,7 @@ def create_fun():
 window = Tk()
 
 window.title('Creating databases')
+window.iconbitmap('document.ico')
 
 main_frm = LabelFrame(window,text = 'Creating Database').grid(row = 0, column = 0, padx = 10, pady = 10)
 
